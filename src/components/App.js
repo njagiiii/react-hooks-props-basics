5646127b7926e010e5b98cd4c3887963c8a8cf87
njagiiii <grace.makena@student.moringaschool.com>
2023-07-19 23:56:37 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import BlogPost from "./BlogPost";
 
 function App() {
-  return <BlogPost />;
+  return (
+    <>
+  <BlogPost />;
+  
+  </>
+  )
+  
 }
 
 export default App;
--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function BlogContent(props) {
   return <div id="blog-content">{props.articleText}</div>;
 }
